e19e905ac26487a19a35967ec16e17b6ae45dd10
Steven Masley <stevenmasley@gmail.com>
2017-05-27 20:44:03 -0500

    Remove unsued

--- app/lender/cmd/main.go
+++ app/lender/cmd/main.go
@@ -12,8 +12,8 @@ var _ = time.Second
 var _ = fmt.Println
 
 var (
-	ACCESS_KEY string = ""
-	SECRET_KEY string = ""
+	ACCESS_KEY string = "GOQI8AZ3-RO5444AI-RM27HI48-AWQ38XSF"
+	SECRET_KEY string = "14a5c36f61d26eb46aed2ad910d7f6f7e7085b36d8ffd2d183074b236e2743fc436942b53b18c06a2d30a271e4e42e683684aeaf62f84ded616747cf9bcc57fd"
 )
 
 func main() {
