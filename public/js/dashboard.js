379cdf5f03a7047d9d8d085dae9d81d4613064da
Steven Masley <stevenmasley@gmail.com>
2017-06-03 17:33:46 -0500

    Move init chart dognut

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -41,6 +41,7 @@ app.controller('dashBaseController', ['$scope', '$http', '$log',
 				console.log(res.data)
 				dashBaseScope.currentUserStats = res.data.CurrentUserStats;
 				dashBaseScope.Balances = res.data.Balances;
+
 				console.log(Math.abs(dashBaseScope.currentUserStats.loanratechange).toFixed(3) <= 0.00)
 			}, (err) => {
 				//error
@@ -169,3 +170,59 @@ app.controller('dashLogsController', ['$scope', '$http', '$log',
 	function($scope, $http, $log) {
 		var dashLogsScope = $scope;
 	}]);
+
+
+
+function init_chart_doughnut(data){
+				
+		if( typeof (Chart) === 'undefined'){ return; }
+		
+		console.log('init_chart_doughnut');
+	 
+		if ($('.canvasDoughnut').length){
+			
+		var chart_doughnut_settings = {
+				type: 'doughnut',
+				tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+				data: {
+					labels: [
+						"Symbian",
+						"Blackberry",
+						"Other",
+						"Android",
+						"IOS"
+					],
+					datasets: [{
+						data: [15, 20, 30, 10, 30],
+						backgroundColor: [
+							"#BDC3C7",
+							"#9B59B6",
+							"#E74C3C",
+							"#26B99A",
+							"#3498DB"
+						],
+						hoverBackgroundColor: [
+							"#CFD4D8",
+							"#B370CF",
+							"#E95E4F",
+							"#36CAAB",
+							"#49A9EA"
+						]
+					}]
+				},
+				options: { 
+					legend: false, 
+					responsive: false 
+				}
+			}
+		
+			$('.canvasDoughnut').each(function(){
+				
+				var chart_element = $(this);
+				var chart_doughnut = new Chart( chart_element, chart_doughnut_settings);
+				
+			});			
+		
+		}  
+	   
+	}
