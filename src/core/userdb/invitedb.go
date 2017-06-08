1cc1ae70a5b0153c9f15c0397401587c4665a4ed
Steven Masley <stevenmasley@gmail.com>
2017-06-07 19:42:51 -0500

    Added invite codes

---
+++ src/core/userdb/invitedb.go
@@ -0,0 +1,246 @@
+package userdb
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/Emyrk/LendingBot/src/core/common/primitives"
+	"github.com/Emyrk/LendingBot/src/core/database"
+)
+
+var (
+	InviteCodeBucket []byte = []byte("Invite_Code_Bucket")
+)
+
+type InviteDB struct {
+	db database.IDatabase
+}
+
+type InviteEntry struct {
+	// Is a hash. Allows for user-readable codes
+	Code [32]byte
+
+	// How many times it can be used
+	Capacity int
+
+	// Usernames who claimed
+	Users []UserEntry
+
+	// Invite code will expire at this time
+	Expires time.Time
+}
+
+func NewInviteDB() *InviteDB {
+	return newInviteDB(false)
+}
+
+func NewInviteMapDB() *InviteDB {
+	return newInviteDB(true)
+}
+
+func newInviteDB(mapDB bool) *InviteDB {
+	i := new(InviteDB)
+	boltpath := os.Getenv("INVITE_DB")
+	if boltpath == "" {
+		boltpath = "InviteUserDB.db"
+	}
+
+	if mapDB {
+		i.db = database.NewMapDB()
+	} else {
+		i.db = database.NewBoltDB(boltpath)
+	}
+
+	return i
+}
+
+func (ie *InviteDB) ClaimInviteCode(username string, code string) (bool, error) {
+	key := sha256.Sum256([]byte(code))
+	data, err := ie.db.Get(InviteCodeBucket, key[:])
+	if err != nil {
+		return false, err
+	}
+
+	if data == nil {
+		return false, fmt.Errorf("not a valid invite code")
+	}
+
+	i := new(InviteEntry)
+	err = i.UnmarshalBinary(data)
+	if err != nil {
+		return false, err
+	}
+
+	if len(i.Users) >= i.Capacity {
+		return false, fmt.Errorf("invite code has been used up")
+	}
+
+	if i.Expires.Before(time.Now()) {
+		return false, fmt.Errorf("invite code has expired")
+	}
+
+	i.Users = append(i.Users, UserEntry{Username: username, ClaimTime: time.Now()})
+	return true, ie.putInviteCode(i)
+}
+
+func (ie *InviteDB) CreateInviteCode(code string, capacity int, expires time.Time) error {
+	i := NewInviteCode(code, capacity, expires)
+	return ie.putInviteCode(i)
+}
+
+func (ie *InviteDB) putInviteCode(ic *InviteEntry) error {
+	data, err := ic.MarshalBinary()
+	if err != nil {
+		return err
+	}
+
+	return ie.db.Put(InviteCodeBucket, ic.Code[:], data)
+}
+
+func NewInviteCode(code string, capacity int, expires time.Time) *InviteEntry {
+	i := new(InviteEntry)
+	i.Code = sha256.Sum256([]byte(code))
+	i.Capacity = capacity
+	i.Expires = expires
+
+	return i
+}
+
+func (ie *InviteEntry) MarshalBinary() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	buf.Write(ie.Code[:])
+
+	buf.Write(primitives.Uint32ToBytes(uint32(ie.Capacity)))
+
+	buf.Write(primitives.Uint32ToBytes(uint32(len(ie.Users))))
+
+	for _, u := range ie.Users {
+		data, err := u.MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(data)
+	}
+
+	data, err := ie.Expires.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	buf.Write(data)
+
+	return buf.Next(buf.Len()), nil
+}
+
+func (ie *InviteEntry) UnmarshalBinary(data []byte) (err error) {
+	_, err = ie.UnmarshalBinaryData(data)
+	return
+}
+
+func (ie *InviteEntry) UnmarshalBinaryData(data []byte) (newData []byte, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("[UserEntry] A panic has occurred while unmarshaling: %s", r)
+			return
+		}
+	}()
+
+	newData = data
+
+	copy(ie.Code[:32], newData[:32])
+	newData = newData[32:]
+
+	u, err := primitives.BytesToUint32(newData[:4])
+	if err != nil {
+		return data, err
+	}
+	newData = newData[4:]
+	ie.Capacity = int(u)
+
+	l, err := primitives.BytesToUint32(newData[:4])
+	if err != nil {
+		return data, err
+	}
+	newData = newData[4:]
+
+	ie.Users = make([]UserEntry, l)
+	for i := 0; i < int(l); i++ {
+		newData, err = ie.Users[i].UnmarshalBinaryData(newData)
+		if err != nil {
+			return data, err
+		}
+	}
+
+	ts := newData[:15]
+	err = ie.Expires.UnmarshalBinary(ts)
+	if err != nil {
+		return data, err
+	}
+
+	newData = newData[15:]
+	return
+}
+
+type UserEntry struct {
+	Username  string
+	ClaimTime time.Time
+}
+
+func NewUserEntry(username string) *UserEntry {
+	u := new(UserEntry)
+	u.Username = username
+	u.ClaimTime = time.Now()
+
+	return u
+}
+
+func (u *UserEntry) MarshalBinary() ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	data, err := primitives.MarshalStringToBytes(u.Username, 100)
+	if err != nil {
+		return nil, err
+	}
+	buf.Write(data)
+
+	data, err = u.ClaimTime.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	buf.Write(data)
+
+	return buf.Next(buf.Len()), nil
+}
+
+func (u *UserEntry) UnmarshalBinary(data []byte) (err error) {
+	_, err = u.UnmarshalBinaryData(data)
+	return
+}
+
+func (u *UserEntry) UnmarshalBinaryData(data []byte) (newData []byte, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("[UserEntry] A panic has occurred while unmarshaling: %s", r)
+			return
+		}
+	}()
+
+	newData = data
+
+	var un string
+	un, newData, err = primitives.UnmarshalStringFromBytesData(newData, 100)
+	if err != nil {
+		return data, err
+	}
+	u.Username = un
+
+	td := newData[:15]
+	err = u.ClaimTime.UnmarshalBinary(td)
+	if err != nil {
+		return data, err
+	}
+	newData = newData[15:]
+	return
+}
