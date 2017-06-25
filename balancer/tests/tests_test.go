ac3566ebe09510707d8b5f0c3b126e960c4bd276
Jesse <jesse.saranwrap@gmail.com>
2017-06-25 05:14:40 -0400

    - commit soem more tests

--- balancer/tests/tests_test.go
+++ balancer/tests/tests_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/Emyrk/LendingBot/balancer"
 	"github.com/Emyrk/LendingBot/bee"
-	"github.com/Emyrk/LendingBot/src/core/database/mongo"
 )
 
 var _, _ = balancer.Shutdown, bee.Online
@@ -38,5 +37,41 @@ func Test_balancer_and_bee_disconnect(t *testing.T) {
 		if b.Status != bee.Shutdown {
 			t.Errorf("Bee not shutdown: %s", b.ID)
 		}
+	}
+}
+
+func Test_balancer_rebalance(t *testing.T) {
+	populateUserTestDB(t)
+
+	bal = balancer.NewBalancer()
+	bal.Run(1151)
+
+	beelist := make([]*bee.Bee, 4, 4)
+	for i := 0; i < 4; i++ {
+		b := bee.NewBee("localhost:1151")
+		beelist = append(beelist, b)
+		b.Run()
+		fmt.Printf("Launched Bee %d\n", i)
+	}
+
+	time.Sleep(500 * time.Millisecond)
+
+	for i, u := range balUsersPOL {
+		bal.AddUser(&u)
+		bal.AddUser(&balUsersBIT[i])
+	}
+
+	time.Sleep(500 * time.Millisecond)
+
+	for i, b := range bal.ConnetionPool.Slaves.GetAllBees() {
+		b.UserLock.RLock()
+		if len(b.Users) != 25 {
+			t.Errorf("Local bee should have quarter of all users: %d", len(b.Users))
+		}
+		b.UserLock.Unlock()
+		if len(beelist[i].Users) != 25 {
+			t.Errorf("Remote bee should have quarter of all users: %d", len(beelist[i].Users))
+		}
+
 	}
 }
