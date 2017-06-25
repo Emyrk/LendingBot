247def7acef083441a4faecc5965f1750fd4c63d
Jesse <jesse.saranwrap@gmail.com>
2017-06-25 03:13:22 -0400

    - trying to test hive

--- balancer/tests/tests_test.go
+++ balancer/tests/tests_test.go
@@ -4,15 +4,17 @@ package tests_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/Emyrk/LendingBot/balancer"
 	"github.com/Emyrk/LendingBot/bee"
 )
 
 var _, _ = balancer.Shutdown, bee.Online
+var bal *balancer.Balancer
 
 func TestBalancerDisconnects(t *testing.T) {
-	bal := balancer.NewBalancer()
+	bal = balancer.NewBalancer()
 	bal.Run(1151)
 
 	beelist := make([]*bee.Bee, 0)
@@ -21,5 +23,17 @@ func TestBalancerDisconnects(t *testing.T) {
 		beelist = append(beelist, b)
 		b.Run()
 		fmt.Printf("Launched Bee %d\n", i)
+	}
+}
+
+func Test_hive_disconnect(t *testing.T) {
+	bal.Close()
+	time.Sleep(500 * time.Nanosecond)
+	bal = balancer.NewBalancer()
+	bal.Run(1151)
+
+	time.Sleep(6 * time.Second)
+	if len(bal.ConnetionPool.Slaves.GetAllBees()) != 10 {
+		t.Errorf("Bees connected after disconnect is only: %d\n", len(bal.ConnetionPool.Slaves.GetAllBees()))
 	}
 }
