7a7a259762c94a9fc413b486146cf6cd1a35b5c6
Steven Masley <stevenmasley@gmail.com>
2017-06-26 18:00:49 -0500

    Added bitfinex objects

---
+++ bee/bitfinexlender/bitfinexlendercmd/main.go
@@ -0,0 +1,75 @@
+// Copyright Andrius Sutas BitfinexLendingBot [at] motoko [dot] sutas [dot] eu
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/Emyrk/LendingBot/bee/bitfinexlender"
+	"github.com/eAndrius/bitfinex-go"
+)
+
+var (
+	configFile  = flag.String("conf", "default.conf", "Configuration file")
+	updateLends = flag.Bool("updatelends", false, "Update lend offerings")
+	dryRun      = flag.Bool("dryrun", false, "Output strategy decisions without placing orders")
+	logToFile   = flag.Bool("logtofile", false, "Write log to file instead of stdout")
+)
+
+func init() {
+	flag.Parse()
+
+	if *logToFile {
+		f, err := os.OpenFile("blb.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			panic("error opening file: " + err.Error())
+		}
+
+		log.SetOutput(f)
+	}
+}
+
+func main() {
+	file, err := os.Open(*configFile)
+	if err != nil {
+		log.Fatal("Failed to open config file: " + err.Error())
+	}
+
+	decoder := json.NewDecoder(file)
+	confs := bitfinexlender.BotConfigs{}
+	err = decoder.Decode(&confs)
+	if err != nil {
+		log.Fatal("Failed to parse config file:" + err.Error())
+	}
+
+	for _, conf := range confs {
+		log.Println("Using Bitfinex user API key: " + conf.Bitfinex.APIKey)
+		conf.API = bitfinex.New(conf.Bitfinex.APIKey, conf.Bitfinex.APISecret)
+
+		balance, err := conf.API.WalletBalances()
+		if err != nil {
+			log.Println("WARNING: Failed to get wallet funds, skipping: " + err.Error())
+			continue
+		}
+
+		activeWallet := strings.ToLower(conf.Bitfinex.ActiveWallet)
+		log.Println("\tDeposit wallet: " +
+			strconv.FormatFloat(balance[bitfinex.WalletKey{"deposit", activeWallet}].Amount, 'f', -1, 64) +
+			" " + activeWallet + " (swappable: " +
+			strconv.FormatFloat(balance[bitfinex.WalletKey{"deposit", activeWallet}].Available, 'f', -1, 64) +
+			" " + activeWallet + ")")
+
+		if *updateLends {
+			err = bitfinexlender.ExecuteStrategy(conf, *dryRun)
+			if err != nil {
+				log.Println("WARNING: Failed to execute strategy: " + err.Error())
+				continue
+			}
+		}
+	}
+}
