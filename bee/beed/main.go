1a401c8f2ed50abeccb594cb7815caac46b6c2c0
Steven Masley <stevenmasley@gmail.com>
2017-07-08 11:40:11 -0500

    Added profiling tobee

--- bee/beed/main.go
+++ bee/beed/main.go
@@ -30,6 +30,7 @@ func main() {
 
 	be := bee.NewBee(*address, *dba, *dbu, pass, *test)
 	be.Run()
+	go StartProfiler()
 
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
