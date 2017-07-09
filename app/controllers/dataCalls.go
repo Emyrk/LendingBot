5324f0fb75733e142eecffd5872944fa3475e5be
Steven Masley <stevenmasley@gmail.com>
2017-07-09 18:49:20 -0500

    Correct names

--- app/controllers/dataCalls.go
+++ app/controllers/dataCalls.go
@@ -162,8 +162,6 @@ func getUserStats(email string) (*CurrentUserStatistics, *UserBalanceDetails) {
 	poloUserStats, err := state.GetUserStatistics(email, 2, "polo")
 	bitUserStats, err := state.GetUserStatistics(email, 2, "bit")
 
-	fmt.Println(poloUserStats)
-
 	collapse := func(data [][]userdb.AllUserStatistic) (*CurrentUserStatistics, *UserBalanceDetails) {
 		balanceDetails := newUserBalanceDetails()
 		today := newCurrentUserStatistics()
