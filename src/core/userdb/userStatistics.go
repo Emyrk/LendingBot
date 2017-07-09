d308137e3165687c5e17fac02e4cad8087a1f25e
Steven Masley <stevenmasley@gmail.com>
2017-07-09 18:34:29 -0500

    correct stats

--- src/core/userdb/userStatistics.go
+++ src/core/userdb/userStatistics.go
@@ -533,7 +533,7 @@ func (us *UserStatisticsDB) GetPoloniexStatistics(currency string) (*PoloniexSta
 			},
 		}
 
-		fmt.Println(len(poloniexStatsArr), t)
+		fmt.Println(len(poloniexStatsArr), t, currency, find)
 		err = c.Find(find).Sort("-_id").All(&poloniexStatsArr)
 		if err != nil {
 			return nil, fmt.Errorf("Mongo: getPoloniexStats: findAll: %s", err.Error())
