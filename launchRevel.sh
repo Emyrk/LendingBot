a1017a76b6d94b8b064534b24de3d002c3c941da
Jesse <jesse.saranwrap@gmail.com>
2017-08-15 01:08:59 -0400

    - added in prod dev setting

--- launchRevel.sh
+++ launchRevel.sh
@@ -6,12 +6,15 @@
 #	3 -> Mongo Revel Password
 #	4 -> Mongo Balancer Pass
 
-if [ "$1" = "prod" ]
-	then
+if [ "$1" = "prod" ] ; then
 	export HODLZONE_KEY=$2
 	export MONGO_REVEL_PASS=$3
 	export MONGO_BAL_PASS=$4
-
+elif [ "$1" = "prodDev" ]; then
+	#Setting is used for testing in prod mode on dev server
+	export HODLZONE_KEY="0766f805c375d84f45554b835377744d92228708"
+	export MONGO_REVEL_PASS="DOESNTMATTER"
+	export MONGO_BAL_PASS="DOESNTMATTER"
 else
 	export HODLZONE_KEY="0000000000000000000000000000000000000000000000000000000000000000"
 	export MONGO_REVEL_PASS=""
