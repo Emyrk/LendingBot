e82d335e30d04fb1fc922b02f75d6ecd8ec2000d
Steven Masley <stevenmasley@gmail.com>
2017-08-16 11:21:16 -0400

    Added collector

---
+++ balancer/collector.go
@@ -0,0 +1,9 @@
+package balancer
+
+// DebtCollector will manage debt and payments
+type DebtCollector struct {
+}
+
+// Listen for coinbase payments
+//		Update status
+// Pay user debts from credits
