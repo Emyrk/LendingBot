d7d34c209376ba349855792cde906ad9d934757d
Jesse <jesse.saranwrap@gmail.com>
2017-08-18 00:07:36 -0400

    - testing

--- src/core/statePayment.go
+++ src/core/statePayment.go
@@ -62,6 +62,7 @@ func (s *State) GetPaymentDebtHistory(username string, limit int) ([]payment.Deb
 
 func (s *State) GetPaymentPaidHistory(username, dateAfterStr string) ([]payment.Paid, error) {
 	var p []payment.Paid
+	fmt.Println("DATEAFTER: ", dateAfterStr == nil, dateAfterStr)
 	if dateAfterStr == "" {
 		//get all payments
 		return s.paymentDB.GetAllPaid(username, nil)
