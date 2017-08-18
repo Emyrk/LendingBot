69044062c4d7ce17e353baf6ef5c32f7f3f18f29
Steven Masley <stevenmasley@gmail.com>
2017-08-17 23:15:53 -0400

    Convert satoshis to btc

--- src/core/payment/util.go
+++ src/core/payment/util.go
@@ -17,3 +17,22 @@ func StringSatoshiFloatToInt64(str string) (int64, error) {
 	}
 	return strconv.ParseInt(parts[0]+parts[1][:8], 10, 64)
 }
+
+func Int64SatoshiToString(val int64) string {
+	neg := ""
+	if val < 0 {
+		val = val * -1
+		neg = "-"
+	}
+	str := fmt.Sprintf("%d", val)
+	if len(str) > 8 { // Place the decimal place where it needs to go
+		end := str[len(str)-8:]
+		beg := str[:len(str)-8]
+		return neg + beg + "." + end
+	}
+
+	for i := len(str); i < 8; i++ {
+		str = "0" + str
+	}
+	return neg + "0." + str
+}
