f11ba1920ee60b8f1a2af335a32fb451f6ae04c1
swrap <jesse.saranwrap@gmail.com>
2018-05-21 17:30:29 -0400

    Changed the db address again

--- bee/beed/main.go
+++ bee/beed/main.go
@@ -16,7 +16,7 @@ import (
 func main() {
 	var (
 		address = flag.String("a", "bee.hodl.zone:9200", "Address to connect to the balancer")
-		dba     = flag.String("dba", "mongo.hodl.zone:34322", "Address for db to connect")
+		dba     = flag.String("dba", "mongodb://mongo1.hodl.zone:4000", "Address for db to connect")
 		dbu     = flag.String("dbu", "bee", "Username for db to connect")
 		test    = flag.Bool("test", false, "Testmode")
 	)
